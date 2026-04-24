672ddad3f036f9e804ff17e20d85cbd97be42665
Luh-code <Lasse@hueffler.de>
2026-04-25 00:10:00 +0200

    ::SDL3

--- flake.nix
+++ flake.nix
@@ -16,12 +16,17 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
         buildInputs = with pkgs; [
           gcc
           clang-tools
           cmake
           ninja
           sdl3
+          shaderc
+          sdl3-shadercross
           pkg-config
           lazygit
           cloc
