2217e07d1c83d5d16cc15e4d7accbdf5aa7f12f2
vinivinoo <antoni.vince92@gmail.com>
2026-09-15 12:41:30 +0200

    added headers for the modules

--- src/libs/blender/blender/modules/modules.hpp
+++ src/libs/blender/blender/modules/modules.hpp
@@ -11,4 +11,65 @@ public:
   using Module::Module;
   auto GenerateTokenString(Out &&out) -> absl::Status;
 };
+
+class FunctionNodeBitMathModule : public ir::Module {
+public:
+  using Module::Module;
+  auto GenerateTokenString(Out &&out) -> absl::Status;
+};
+
+class FunctionNodeCompareModule : public ir::Module {
+public:
+  using Module::Module;
+  auto GenerateTokenString(Out &&out) -> absl::Status;
+};
+
+class FunctionNodeHashValueModule : public ir::Module {
+public:
+  using Module::Module;
+  auto GenerateTokenString(Out &&out) -> absl::Status;
+};
+
+class FunctionIntegerMathModule : public ir::Module {
+public:
+  using Module::Module;
+  auto GenerateTokenString(Out &&out) -> absl::Status;
+};
+
+class ShaderNodeBooleanMathModule : public ir::Module {
+public:
+  using Module::Module;
+  auto GenerateTokenString(Out &&out) -> absl::Status;
+};
+
+class ShaderNodeClampModule : public ir::Module {
+public:
+  using Module::Module;
+  auto GenerateTokenString(Out &&out) -> absl::Status;
+};
+
+class ShaderNodeFloatCurveModule : public ir::Module {
+public:
+  using Module::Module;
+  auto GenerateTokenString(Out &&out) -> absl::Status;
+};
+
+class ShaderNodeMapRangeModule : public ir::Module {
+public:
+  using Module::Module;
+  auto GenerateTokenString(Out &&out) -> absl::Status;
+};
+
+class ShaderNodeMathModule : public ir::Module {
+public:
+  using Module::Module;
+  auto GenerateTokenString(Out &&out) -> absl::Status;
+};
+
+class ShaderMathMixModule : public ir::Module {
+public:
+  using Module::Module;
+  auto GenerateTokenString(Out &&out) -> absl::Status;
+};
+
 } // namespace msk::blender
