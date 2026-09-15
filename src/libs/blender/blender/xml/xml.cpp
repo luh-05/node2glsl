c4df9f34d17856a15785993a11db4c66f0be36a4
Tom Schäfer <tschaefer.acc@gmail.com>
2026-09-15 19:50:02 +0200

    pugixml setup

--- src/libs/blender/blender/xml/xml.cpp
+++ src/libs/blender/blender/xml/xml.cpp
@@ -1,1 +1,14 @@
 #include "blender/xml/xml.hpp"
+#include "blender/ir_shim/ir_shim.hpp"
+#include <pugixml.hpp>
+
+namespace msk::blender {
+
+
+auto XMLParser::XMLread(std::string_view path) -> absl::Status {
+
+
+    return absl::OkStatus();
+}
+
+}
--- src/libs/blender/blender/xml/xml.hpp
+++ src/libs/blender/blender/xml/xml.hpp
@@ -1,3 +1,14 @@
 #pragma once
+#include <string>
 
-namespace msk::blender {}
+namespace msk::blender {
+
+class XMLParser {
+    pugi::xml_parse_result parse_result;
+
+public:
+ auto XMLread(std::string_view path) -> absl::Status;
+ auto ParseGraph(std::string_view target_graph_id) -> absl::StatusOr<const GraphHandle>;
+};
+
+}
