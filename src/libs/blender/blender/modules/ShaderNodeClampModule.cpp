dc10e890386b60fab92026acbd47fd1de06649c8
vinivinoo <antoni.vince92@gmail.com>
2026-09-16 19:37:43 +0200

    cleaned up ShaderNodeClampModule.cpp

--- src/libs/blender/blender/modules/ShaderNodeClampModule.cpp
+++ src/libs/blender/blender/modules/ShaderNodeClampModule.cpp
@@ -9,44 +9,37 @@
 
 namespace msk::blender {
 
-#define TextToken(text) out.legacy->CreateMTT(text)
-#define WildcardToken(p, name) out.legacy->CreateMWT(p, name)
-
 auto ShaderNodeClampModule::GenerateTokenString(Out &&out) -> absl::Status {
   auto clamp_type = out.GetConstant<std::string>("clamp_type0");
 
-  if (clamp_type == "RANGE") {
-    out.legacy->AddTokenVector({
-        WildcardToken(Out::RIGHT, "Result0"),
-        TextToken(" = clamp("),
-        WildcardToken(Out::LEFT, "Value0"),
-        TextToken(", min("),
-        WildcardToken(Out::LEFT, "Min0"),
-        TextToken(", "),
-        WildcardToken(Out::LEFT, "Max0"),
-        TextToken("), max("),
-        WildcardToken(Out::LEFT, "Min0"),
-        TextToken(", "),
-        WildcardToken(Out::LEFT, "Max0"),
-        TextToken("));")
-    });
-  } else {
-    out.legacy->AddTokenVector({
-        WildcardToken(Out::RIGHT, "Result0"),
-        TextToken(" = clamp("),
-        WildcardToken(Out::LEFT, "Value0"),
-        TextToken(", "),
-        WildcardToken(Out::LEFT, "Min0"),
-        TextToken(", "),
-        WildcardToken(Out::LEFT, "Max0"),
-        TextToken(");")
-    });
+  if (clamp_type == "MINMAX") {
+    out + Out::RIGHT / "Result0"
+        + " = clamp("
+        + Out::LEFT / "Value0"
+        + ", "
+        + Out::LEFT / "Min0"
+        + ", "
+        + Out::LEFT / "Max0"
+        + ");";
+
+    return out.GetStatus();
+  } 
+  
+  else if (clamp_type == "RANGE") {
+    out + Out::RIGHT / "Result0"
+        + " = clamp("
+        + Out::LEFT / "Value0"
+        + ", min(" + Out::LEFT / "Min0" + ", " + Out::LEFT / "Max0" + ")"
+        + ", max(" + Out::LEFT / "Min0" + ", " + Out::LEFT / "Max0" + "));";
+
+    return out.GetStatus();
+  } 
+  
+  else {
+    return absl::InvalidArgumentError(
+      std::format("Illegal value of clamp_type constant: '{}'", clamp_type));
   }
 
-  return out.GetStatus();
 }
-
-#undef TextToken
-#undef WildcardToken
 
 } // namespace msk::blender
