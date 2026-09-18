cc51a041a4f36a8982c9aca563863b370341e9fd
vinivinoo <antoni.vince92@gmail.com>
2026-09-18 23:03:33 +0200

    fehlendes return statement hinzugefügt

--- src/libs/blender/blender/modules/FunctionNodeIntegerMath.cpp
+++ src/libs/blender/blender/modules/FunctionNodeIntegerMath.cpp
@@ -68,7 +68,6 @@ auto FunctionNodeIntegerMath::GenerateTokenString(Out &&out) -> absl::Status {
   }
 
   else if (op_c == "GCD") {
-    
 
     out + "{";
     out + "if(" + Out::LEFT / "Value0" + "= 0 or" + Out::LEFT / "Value1" +
@@ -89,20 +88,16 @@ auto FunctionNodeIntegerMath::GenerateTokenString(Out &&out) -> absl::Status {
     out + Out::RIGHT / "Value3" + "= result;";
     out + "}";
 
+    return out.GetStatus();
   }
 
   else if (op_c == "LCM") {
     out + "{";
-
     out + "int lcm_a = abs(" + Out::LEFT / "Value0" + ");";
-
     out + "int lcm_b = abs(" + Out::LEFT / "Value1" + ");";
-
     out + "int lcm_x = lcm_a;" + "int lcm_y = lcm_b;";
-
     out + "while (lcm_y != 0) {" + "int lcm_tmp = lcm_x % lcm_y;" +
         "lcm_x = lcm_y;" + "lcm_y = lcm_tmp;" + "}";
-
     out + Out::RIGHT / "Value3" +
         " = (lcm_x == 0) ? 0 : abs((lcm_a / lcm_x) * lcm_b);";
     out + "}";
