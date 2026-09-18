b648f1707183b8e56cf407ebcf41abfe5070db20
helena-laptop <absolutelyjooking@gmail.com>
2026-09-19 00:13:35 +0200

    bool

--- src/libs/blender/blender/modules/inputs/FunctionNodeInputInt.cpp
+++ src/libs/blender/blender/modules/inputs/FunctionNodeInputInt.cpp
@@ -9,9 +9,9 @@
 namespace msk::blender {
 
 auto DummyModule::GenerateTokenString(Out &&out) -> absl::Status {
-  auto number = out.GetConstant<std::string>("integer0");
+  auto integer_var = out.GetConstant<std::string>("integer0");
 
-  out + Out::RIGHT / "Integer0" + "=" + number + ";";
+  out + Out::RIGHT / "Integer0" + "=" + integer_var + ";";
 
   return out.GetStatus();
 }
