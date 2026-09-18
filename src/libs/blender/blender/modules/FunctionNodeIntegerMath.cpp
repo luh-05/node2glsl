da93d6c06b6813eec409ffe4f9660b8c87201880
vinivinoo <antoni.vince92@gmail.com>
2026-09-18 22:47:12 +0200

    added LCM

--- src/libs/blender/blender/modules/FunctionNodeIntegerMath.cpp
+++ src/libs/blender/blender/modules/FunctionNodeIntegerMath.cpp
@@ -157,6 +157,32 @@ auto FunctionNodeIntegerMath::GenerateTokenString(Out &&out)
         + Out::LEFT / "Value1"
         + ");";
   } 
+  else if (op_c == "LCM") {
+    out + "{";
+
+    out + "int lcm_a = abs("
+        + Out::LEFT / "Value0"
+        + ");";
+
+    out + "int lcm_b = abs("
+        + Out::LEFT / "Value1"
+        + ");";
+
+    out + "int lcm_x = lcm_a;"
+        + "int lcm_y = lcm_b;";
+
+    out + "while (lcm_y != 0) {"
+        + "int lcm_tmp = lcm_x % lcm_y;"
+        + "lcm_x = lcm_y;"
+        + "lcm_y = lcm_tmp;"
+        + "}";
+
+    out + Out::RIGHT / "Value3"
+        + " = (lcm_x == 0) ? 0 : abs((lcm_a / lcm_x) * lcm_b);";
+    out + "}";
+
+    return out.GetStatus();
+  }
   else {
     return absl::InvalidArgumentError(
       std::format("Illegal value of operation constant in Integer Math: '{}'", op_c));
