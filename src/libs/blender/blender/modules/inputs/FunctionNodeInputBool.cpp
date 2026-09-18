b79edf703b507bafb04f807dd0cd0ab3365651ef
vinivinoo <antoni.vince92@gmail.com>
2026-09-19 01:23:18 +0200

    eingerückt

--- src/libs/blender/blender/modules/inputs/FunctionNodeInputBool.cpp
+++ src/libs/blender/blender/modules/inputs/FunctionNodeInputBool.cpp
@@ -1,5 +1,5 @@
-#include "mir/node_graph/node_graph.hpp"
 #include "blender/modules/modules.hpp"
+#include "mir/node_graph/node_graph.hpp"
 #include <absl/status/status.h>
 #include <absl/status/statusor.h>
 #include <array>
@@ -9,11 +9,11 @@
 namespace msk::blender {
 
 auto FunctionNodeInputBool::GenerateTokenString(Out &&out) -> absl::Status {
-auto boolean_var = out.GetConstant<std::string>("boolean0");
-  
-out + Out::RIGHT / "Boolean0" + "=" + boolean_var + ";";
+  auto boolean_var = out.GetConstant<std::string>("boolean0");
 
-return out.GetStatus();
+  out + Out::RIGHT / "Boolean0" + "=" + boolean_var + ";";
+
+  return out.GetStatus();
 }
 
 } // namespace msk::blender
