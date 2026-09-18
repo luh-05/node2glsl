a74bbbd2f65cfd46abb1950888376d23cc989a46
vinivinoo <antoni.vince92@gmail.com>
2026-09-18 19:17:41 +0200

    cleaned up ShaderNodeMapRangeModule.cpp

--- src/libs/blender/blender/modules/ShaderNodeMapRangeModule.cpp
+++ src/libs/blender/blender/modules/ShaderNodeMapRangeModule.cpp
@@ -2,105 +2,245 @@
 #include "modules.hpp"
 #include <absl/status/status.h>
 #include <absl/status/statusor.h>
-#include <array>
-#include <memory>
 #include <string>
+#include <format>
 #include <mir/codegen.hpp>
 
 namespace msk::blender {
 
-#define TextToken(text) out.legacy->CreateMTT(text)
-#define WildcardToken(p, name) out.legacy->CreateMWT(p, name)
-
 auto ShaderNodeMapRangeModule::GenerateTokenString(Out &&out) -> absl::Status {
   auto data_type = out.GetConstant<std::string>("data_type0");
-  auto interp_type = out.GetConstant<std::string>("interpolation_type0");
-  auto clamp = out.GetConstant<std::string>("clamp0");
+  auto interp    = out.GetConstant<std::string>("interpolation_type0");
+  auto clamp_val = out.GetConstant<std::string>("clamp0");
 
-  // Port names dependent on data type
-  std::string val_in;
-  std::string from_min;
-  std::string from_max;
-  std::string to_min;
-  std::string to_max;
-  std::string steps;
-  std::string result_out;
+  std::string p_val, p_fmin, p_fmax, p_tmin, p_tmax, p_steps, p_out;
 
-  if (data_type == "FLOAT_VECTOR") {
-    val_in = "Vector0";
-    from_min = "From Min1";
-    from_max = "From Max1";
-    to_min = "To Min1";
-    to_max = "To Max1";
-    steps = "Steps1";
-    result_out = "Vector1";
-  } else {
-    // Default / Float fallback
-    val_in = "Value0";
-    from_min = "From Min0";
-    from_max = "From Max0";
-    to_min = "To Min0";
-    to_max = "To Max0";
-    steps = "Steps0";
-    result_out = "Result0";
+  if (data_type == "FLOAT") {
+    p_val   = "Value0";
+    p_fmin  = "From Min0";
+    p_fmax  = "From Max0";
+    p_tmin  = "To Min0";
+    p_tmax  = "To Max0";
+    p_steps = "Steps0";
+    p_out   = "Result0";
+  } 
+  else if (data_type == "FLOAT_VECTOR") {
+    p_val   = "Vector0";
+    p_fmin  = "From Min1";
+    p_fmax  = "From Max1";
+    p_tmin  = "To Min1";
+    p_tmax  = "To Max1";
+    p_steps = "Steps1";
+    p_out   = "Vector0";
+  } 
+  else {
+    return absl::InvalidArgumentError(
+        std::format("Unsupported data_type for Map Range: '{}'", data_type));
   }
 
-  // Dynamic target function name construct
-  std::string func_name = "map_range_";
+  if (interp == "LINEAR") {
+    if (clamp_val == "True") {
+      out + Out::RIGHT / p_out
+          + " = clamp("
+          + Out::LEFT / p_tmin
+          + " + ("
+          + Out::LEFT / p_val
+          + " - "
+          + Out::LEFT / p_fmin
+          + ") / ("
+          + Out::LEFT / p_fmax
+          + " - "
+          + Out::LEFT / p_fmin
+          + ") * ("
+          + Out::LEFT / p_tmax
+          + " - "
+          + Out::LEFT / p_tmin
+          + "), min("
+          + Out::LEFT / p_tmin
+          + ", "
+          + Out::LEFT / p_tmax
+          + "), max("
+          + Out::LEFT / p_tmin
+          + ", "
+          + Out::LEFT / p_tmax
+          + "));";
+    } else {
+      out + Out::RIGHT / p_out
+          + " = "
+          + Out::LEFT / p_tmin
+          + " + ("
+          + Out::LEFT / p_val
+          + " - "
+          + Out::LEFT / p_fmin
+          + ") / ("
+          + Out::LEFT / p_fmax
+          + " - "
+          + Out::LEFT / p_fmin
+          + ") * ("
+          + Out::LEFT / p_tmax
+          + " - "
+          + Out::LEFT / p_tmin
+          + ");";
+    }
+  } 
+  
+  else if (interp == "STEPPED") {
+    if (clamp_val == "True") {
+      out + Out::RIGHT / p_out
+          + " = clamp("
+          + Out::LEFT / p_tmin
+          + " + floor(("
+          + Out::LEFT / p_val
+          + " - "
+          + Out::LEFT / p_fmin
+          + ") / ("
+          + Out::LEFT / p_fmax
+          + " - "
+          + Out::LEFT / p_fmin
+          + ") * ("
+          + Out::LEFT / p_steps
+          + " + 1.0)) / "
+          + Out::LEFT / p_steps
+          + " * ("
+          + Out::LEFT / p_tmax
+          + " - "
+          + Out::LEFT / p_tmin
+          + "), min("
+          + Out::LEFT / p_tmin
+          + ", "
+          + Out::LEFT / p_tmax
+          + "), max("
+          + Out::LEFT / p_tmin
+          + ", "
+          + Out::LEFT / p_tmax
+          + "));";
+    } else {
+      out + Out::RIGHT / p_out
+          + " = "
+          + Out::LEFT / p_tmin
+          + " + floor(("
+          + Out::LEFT / p_val
+          + " - "
+          + Out::LEFT / p_fmin
+          + ") / ("
+          + Out::LEFT / p_fmax
+          + " - "
+          + Out::LEFT / p_fmin
+          + ") * ("
+          + Out::LEFT / p_steps
+          + " + 1.0)) / "
+          + Out::LEFT / p_steps
+          + " * ("
+          + Out::LEFT / p_tmax
+          + " - "
+          + Out::LEFT / p_tmin
+          + ");";
+    }
+  } 
+  
+  else if (interp == "SMOOTHSTEP") {
+    if (clamp_val == "True") {
+      out + Out::RIGHT / p_out
+          + " = clamp("
+          + Out::LEFT / p_tmin
+          + " + smoothstep(0.0, 1.0, ("
+          + Out::LEFT / p_val
+          + " - "
+          + Out::LEFT / p_fmin
+          + ") / ("
+          + Out::LEFT / p_fmax
+          + " - "
+          + Out::LEFT / p_fmin
+          + ")) * ("
+          + Out::LEFT / p_tmax
+          + " - "
+          + Out::LEFT / p_tmin
+          + "), min("
+          + Out::LEFT / p_tmin
+          + ", "
+          + Out::LEFT / p_tmax
+          + "), max("
+          + Out::LEFT / p_tmin
+          + ", "
+          + Out::LEFT / p_tmax
+          + "));";
+    } else {
+      out + Out::RIGHT / p_out
+          + " = "
+          + Out::LEFT / p_tmin
+          + " + smoothstep(0.0, 1.0, ("
+          + Out::LEFT / p_val
+          + " - "
+          + Out::LEFT / p_fmin
+          + ") / ("
+          + Out::LEFT / p_fmax
+          + " - "
+          + Out::LEFT / p_fmin
+          + ")) * ("
+          + Out::LEFT / p_tmax
+          + " - "
+          + Out::LEFT / p_tmin
+          + ");";
+    }
+  } 
+  
+  else if (interp == "SMOOTHERSTEP") {
 
-  if (interp_type == "LINEAR") {
-    func_name += "linear";
-  } else if (interp_type == "STEPPED") {
-    func_name += "stepped";
-  } else if (interp_type == "SMOOTHSTEP") {
-    func_name += "smoothstep";
-  } else if (interp_type == "SMOOTHERSTEP") {
-    func_name += "smootherstep";
-  }
-
-  if (clamp == "True") {
-    func_name += "_clamp";
-  }
-
-  // STEPPED interpolation requires the extra "Steps" parameter
-  if (interp_type == "STEPPED") {
-    out.legacy->AddTokenVector({
-        WildcardToken(Out::RIGHT, result_out),
-        TextToken(" = " + func_name + "("),
-        WildcardToken(Out::LEFT, val_in),
-        TextToken(", "),
-        WildcardToken(Out::LEFT, from_min),
-        TextToken(", "),
-        WildcardToken(Out::LEFT, from_max),
-        TextToken(", "),
-        WildcardToken(Out::LEFT, to_min),
-        TextToken(", "),
-        WildcardToken(Out::LEFT, to_max),
-        TextToken(", "),
-        WildcardToken(Out::LEFT, steps),
-        TextToken(");")
-    });
-  } else {
-    out.legacy->AddTokenVector({
-        WildcardToken(Out::RIGHT, result_out),
-        TextToken(" = " + func_name + "("),
-        WildcardToken(Out::LEFT, val_in),
-        TextToken(", "),
-        WildcardToken(Out::LEFT, from_min),
-        TextToken(", "),
-        WildcardToken(Out::LEFT, from_max),
-        TextToken(", "),
-        WildcardToken(Out::LEFT, to_min),
-        TextToken(", "),
-        WildcardToken(Out::LEFT, to_max),
-        TextToken(");")
-    });
+    if (clamp_val == "True") {
+      out + Out::RIGHT / p_out
+          + " = clamp("
+          + Out::LEFT / p_tmin
+          + " + ("
+          + "clamp((" + Out::LEFT / p_val + " - " + Out::LEFT / p_fmin + ") / (" + Out::LEFT / p_fmax + " - " + Out::LEFT / p_fmin + "), 0.0, 1.0)"
+          + " * " 
+          + "clamp((" + Out::LEFT / p_val + " - " + Out::LEFT / p_fmin + ") / (" + Out::LEFT / p_fmax + " - " + Out::LEFT / p_fmin + "), 0.0, 1.0)"
+          + " * " 
+          + "clamp((" + Out::LEFT / p_val + " - " + Out::LEFT / p_fmin + ") / (" + Out::LEFT / p_fmax + " - " + Out::LEFT / p_fmin + "), 0.0, 1.0)"
+          + " * (" 
+          + "clamp((" + Out::LEFT / p_val + " - " + Out::LEFT / p_fmin + ") / (" + Out::LEFT / p_fmax + " - " + Out::LEFT / p_fmin + "), 0.0, 1.0)"
+          + " * (" 
+          + "clamp((" + Out::LEFT / p_val + " - " + Out::LEFT / p_fmin + ") / (" + Out::LEFT / p_fmax + " - " + Out::LEFT / p_fmin + "), 0.0, 1.0)"
+          + " * 6.0 - 15.0) + 10.0)) * ("
+          + Out::LEFT / p_tmax
+          + " - "
+          + Out::LEFT / p_tmin
+          + "), min("
+          + Out::LEFT / p_tmin
+          + ", "
+          + Out::LEFT / p_tmax
+          + "), max("
+          + Out::LEFT / p_tmin
+          + ", "
+          + Out::LEFT / p_tmax
+          + "));";
+    } else {
+      out + Out::RIGHT / p_out
+          + " = "
+          + Out::LEFT / p_tmin
+          + " + ("
+          + "clamp((" + Out::LEFT / p_val + " - " + Out::LEFT / p_fmin + ") / (" + Out::LEFT / p_fmax + " - " + Out::LEFT / p_fmin + "), 0.0, 1.0)"
+          + " * " 
+          + "clamp((" + Out::LEFT / p_val + " - " + Out::LEFT / p_fmin + ") / (" + Out::LEFT / p_fmax + " - " + Out::LEFT / p_fmin + "), 0.0, 1.0)"
+          + " * " 
+          + "clamp((" + Out::LEFT / p_val + " - " + Out::LEFT / p_fmin + ") / (" + Out::LEFT / p_fmax + " - " + Out::LEFT / p_fmin + "), 0.0, 1.0)"
+          + " * (" 
+          + "clamp((" + Out::LEFT / p_val + " - " + Out::LEFT / p_fmin + ") / (" + Out::LEFT / p_fmax + " - " + Out::LEFT / p_fmin + "), 0.0, 1.0)"
+          + " * (" 
+          + "clamp((" + Out::LEFT / p_val + " - " + Out::LEFT / p_fmin + ") / (" + Out::LEFT / p_fmax + " - " + Out::LEFT / p_fmin + "), 0.0, 1.0)"
+          + " * 6.0 - 15.0) + 10.0)) * ("
+          + Out::LEFT / p_tmax
+          + " - "
+          + Out::LEFT / p_tmin
+          + ");";
+    }
+  } 
+  
+  else {
+    return absl::InvalidArgumentError(
+        std::format("Unknown interpolation type for Map Range: '{}'", interp));
   }
 
   return out.GetStatus();
 }
-
-#undef TextToken
-#undef WildcardToken
 
 } // namespace msk::blender
